e05c5faf4d3b6c8a7b2d6e29d91edf66799d4161
Tan Vir <tanvirulislam.niloy@gmail.com>
2023-03-29 00:21:11 +0600

    Update main.cpp

--- Lab 02/Task 1 and 2/main.cpp
+++ Lab 02/Task 1 and 2/main.cpp
@@ -1,1 +1,35 @@
+// For Task 1 and 2
+#include <iostream>
+#include "dynarr.h"
+using namespace std;
 
+int main()
+{
+    // Task 1.1
+    dynArr object1(), object2(5);
+
+    // Task 1.2
+    cout << "Enter 5 int value: ";
+    for ( int i = 0; i < 5; i++ ) {
+        int temp;
+        cin >> temp;
+        object2.setValue (i, temp);
+    }
+
+    // Task 1.3
+    cout << "The values are: ";
+    for ( int i = 0; i < 5; i++ ) {
+        cout << object2.getValue (i) << " " ;
+    }
+
+    // Task 2
+    cout << "\nEnter new size of the array: ";
+    int s;
+    cin >> s;
+    object2.allocate(s);
+    cout << "Values of new array: ";
+    for ( int i = 0; i < s; i++ ) {
+        cout << object2.getValue (i) << " " ;
+    }
+    return 0;
+}
